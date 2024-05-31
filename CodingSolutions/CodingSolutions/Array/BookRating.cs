1f6a7ab558023d78eddb16a7d5f76a66bfedbb61
Ashish Agarwal <quaintashish@gmail.com>
2024-05-31 12:32:04 +0530

    temp

---
+++ CodingSolutions/CodingSolutions/Array/BookRating.cs
@@ -0,0 +1,61 @@
+﻿public class BookRatings
+{
+    private List<string> books;
+    private List<string> genres;
+    private List<int> ratings;
+    
+    public BookRatings(List<string> books, List<string> genres, List<int> ratings)
+    {
+        if (books.Count != genres.Count || genres.Count != ratings.Count)
+        {
+            throw new ArgumentException("All lists must have the same length");
+        }
+
+        this.books = new List<string>(books);
+        this.genres = new List<string>(genres);
+        this.ratings = new List<int>(ratings);
+    }
+
+    public string GetHighestRatingBookByGenre(string genre)
+    {
+        var genreBooks = new List<Tuple<string, int>>();
+
+        for (int i = 0; i < genres.Count; i++)
+        {
+            if (genres[i] == genre)
+            {
+                genreBooks.Add(new Tuple<string, int>(books[i], ratings[i]));
+            }
+        }
+
+        if (genreBooks.Count == 0)
+        {
+            return null; // or throw an exception or return a default value
+        }
+
+        genreBooks.Sort((a, b) =>
+        {
+            int ratingComparison = b.Item2.CompareTo(a.Item2);
+            if (ratingComparison == 0)
+            {
+                return a.Item1.CompareTo(b.Item1);
+            }
+            return ratingComparison;
+        });
+
+        return genreBooks[0].Item1;
+    }
+
+    public void UpdateBookRatingByBookName(string bookName, int rating)
+    {
+        int index = books.IndexOf(bookName);
+        if (index != -1)
+        {
+            ratings[index] = rating;
+        }
+        else
+        {
+            throw new ArgumentException("Book not found");
+        }
+    }
+}
---
+++ CodingSolutions/CodingSolutionsTest/Array/BookRatingsTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+
+namespace CodingSolutionsTest.Array
+{
+        /*
+
+    List of books, list of Genre of books and book ratings are given, all list were of equal length.
+    book[], genre[], rating[] of equal length n
+
+    Now there were 2 methods which we need to implement,
+    1. getHighestRatingBookByGenre("Genre_name") If same rating books then lexographical order
+    2. updateBookRatingbyBookName("book_name", int rating)
+
+
+
+    */
+
+    public class BookRatingsTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            List<string> books = new List<string> { "BookA", "BookB", "BookC", "BookD" };
+            List<string> genres = new List<string> { "Fiction", "Non-Fiction", "Fiction", "Fiction" };
+            List<int> ratings = new List<int> { 5, 3, 5, 4 };
+
+            BookRatings bookRatings = new BookRatings(books, genres, ratings);
+
+            string highestRatedFiction = bookRatings.GetHighestRatingBookByGenre("Fiction");
+            highestRatedFiction.Should().Be("BookA");
+
+            bookRatings.UpdateBookRatingByBookName("BookB", 5);
+            string highestRatedNonFiction = bookRatings.GetHighestRatingBookByGenre("Non-Fiction");
+            highestRatedNonFiction.Should().Be("BookB");
+        }
+    }
+
+}
